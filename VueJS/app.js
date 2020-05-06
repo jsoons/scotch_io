63121b8fdd24653da38a5c7b73c367129e46b69c
jsoons <jsoons@gmail.com>
2020-05-06 22:48:58 +0200

    vue input

--- VueJS/app.js
+++ VueJS/app.js
@@ -2,5 +2,11 @@ new Vue({
   el: "#app",
   data: {
     title: "Hello World."
+  },
+  methods: {
+    changeTitle: function (event) {
+      // console.log(event, this);
+      this.title = event.target.value;
+    }
   }
 });
