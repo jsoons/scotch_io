12beed8e95a85488a11e2cbec906b0278df5f278
jsoons <jsoons@gmail.com>
2020-05-07 14:40:02 +0200

    red

--- VueJS/app.js
+++ VueJS/app.js
@@ -1,12 +1,32 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Hello World."
+    title: "Hello World.",
+    link: "http://google.com",
+    finishedLink: '<a href="http://google.com">Google</a>',
+    crazyString: "<a href=\"http://google.com\">Google</a>",
+    counter: 0,
+    x: 0,
+    y: 0,
+    model: "Model"
   },
   methods: {
     changeTitle: function (event) {
       // console.log(event, this);
       this.title = event.target.value;
+    },
+    sayHello: function() {
+      // return("Hi Hi Hi");
+      return this.title;
+
+    },
+    increase: function(step) {
+      this.counter+=step;
+    }, 
+    updateCoordinates: function(e) {
+      this.x= e.clientX;
+      this.y= e.clientY;
+
     }
   }
 });
